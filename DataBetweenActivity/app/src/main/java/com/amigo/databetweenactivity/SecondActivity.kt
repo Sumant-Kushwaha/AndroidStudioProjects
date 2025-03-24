8729a4c07a7d80cef97cdf16253f41544b337506
Sumant-Kushwaha <araj482367@gmail.com>
2025-03-25 00:02:16 +0530

    Passing Data between Activity

---
+++ DataBetweenActivity/app/src/main/java/com/amigo/databetweenactivity/SecondActivity.kt
@@ -0,0 +1,27 @@
+package com.amigo.databetweenactivity
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val name=intent.getStringExtra("NAME")
+        val age=intent.getIntExtra("AGE",0)
+        val country=intent.getStringExtra("COUNTRY")
+
+        val outputString="I am $name  a $age year old student lives in $country"
+        findViewById<TextView>(R.id.tvOutput).text=outputString
+    }
+}
